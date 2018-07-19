8a30461d1cde81bc80f886edfb87235d179c6ebc
usuario <limbertx@hotmail.com>
2018-07-19 16:34:02 -0400

    cambio a postgres heroku

--- config/db.php
+++ config/db.php
@@ -3,11 +3,38 @@
 return [
     'class' => 'yii\db\Connection',
 
-    //'dsn'=>'pgsql:host=ec2-54-235-219-113.compute-1.amazonaws.com;port=5432;dbname=d43bk4fjq9s3o3',
+    'dsn'=>'pgsql:host=ec2-174-129-192-200.compute-1.amazonaws.com;port=5432;dbname=d3fnee10sio9nf',
 
-	'dsn'=>'pgsql:host=localhost;port=5432;dbname=dbtaller1',
-    'username' => 'odoo',
-    'password' => 'odoo',
-
+	//'dsn'=>'pgsql:host=localhost;port=5432;dbname=dbtaller1',
+    //'username' => 'odoo',
+    //'password' => 'odoo',
+    'username' => 'dzzscukjmwamns',
+ 'password' => '50ac6f6983efa891fa6c640d7854c7353ef8331bebea379b802b15fd3d9d3901',
     'charset' => 'utf8',
 ];
+
+
+/**
+
+
+
+Host
+    ec2-174-129-192-200.compute-1.amazonaws.com
+Database
+    d3fnee10sio9nf
+User
+    dzzscukjmwamns
+Port
+    5432
+Password
+    50ac6f6983efa891fa6c640d7854c7353ef8331bebea379b802b15fd3d9d3901
+URI
+    postgres://dzzscukjmwamns:50ac6f6983efa891fa6c640d7854c7353ef8331bebea379b802b15fd3d9d3901@ec2-174-129-192-200.compute-1.amazonaws.com:5432/d3fnee10sio9nf
+
+
+
+
+
+
+
+**/
